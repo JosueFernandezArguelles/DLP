84ab7c5ce87dbc57021e80ebed52610cdef8db57
Josué Argüelles <UO277912@uniovi.es>
2024-03-04 17:31:09 +0100

    Remove unnecessary implements

--- src/ast/program/Program.java
+++ src/ast/program/Program.java
@@ -6,7 +6,7 @@ import ast.AbstractASTNode;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Program extends AbstractASTNode implements ASTNode {
+public class Program extends AbstractASTNode {
 
     private List<Definition> definitions;
     public Program(int line, int column, List<Definition> definitions) {
